9ac6320d7b496c919489aedba80525da41b7fe2e
Danylo Senchyshyn <danilsencisin@gmail.com>
2025-03-15 20:45:33 +0100

    added data base

---
+++ src/main/sk/tuke/kpi/kp/checkers/service/RatingService.java
@@ -0,0 +1,12 @@
+package sk.tuke.kpi.kp.checkers.service;
+
+import sk.tuke.kpi.kp.checkers.entity.Rating;
+
+import java.util.List;
+
+public interface RatingService {
+    void setRating(Rating rating) throws RatingException;
+    int getAverageRating(String game) throws RatingException;
+    int getRating(String game, String player) throws RatingException;
+    void reset() throws RatingException;
+}
