bc575459a5a8c9791dc04d68d5fe7ba142327b32
Danylo Senchyshyn <danilsencisin@gmail.com>
2025-03-20 22:12:47 +0100

    fix

---
+++ src/main/java/sk/tuke/kpi/kp/game/checkers/core/GameState.java
@@ -0,0 +1,5 @@
+package sk.tuke.kpi.kp.game.checkers.core;
+
+public enum GameState {
+    PLAYING, WHITE_WON, BLACK_WON, DRAW;
+}
---
+++ src/test/java/sk/tuke/gamestudio/service/CommentServiceTest.java
@@ -0,0 +1,80 @@
+//package sk.tuke.gamestudio.service;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import sk.tuke.kpi.kp.entity.Comment;
+//import sk.tuke.kpi.kp.service.CommentService;
+//import sk.tuke.kpi.kp.service.CommentServiceJDBC;
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//public class CommentServiceTest {
+//    CommentService commentService;
+//
+//    @Before
+//    public void setUp() {
+//        commentService = new CommentServiceJDBC();
+//    }
+//
+//    @Test
+//    public void addComment() {
+//        commentService.reset();
+//
+//        Date date = new Date();
+//        commentService.addComment(new Comment("checkers", "player1", "Nice game!", date));
+//
+//        List<Comment> comments = commentService.getComments("checkers");
+//        assertEquals(1, comments.size());
+//        assertEquals("checkers", comments.get(0).getGame());
+//        assertEquals("player1", comments.get(0).getPlayer());
+//        assertEquals("Nice game!", comments.get(0).getComment());
+//        assertEquals(date, comments.get(0).getCommentedOn());
+//
+//        commentService.reset();
+//    }
+//
+//    @Test
+//    public void getComments() {
+//        commentService.reset();
+//
+//        Date date = new Date();
+//        commentService.addComment(new Comment("checkers", "player1", "Nice game!", date));
+//        commentService.addComment(new Comment("checkers", "player2", "Awesome!", date));
+//
+//        List<Comment> comments = commentService.getComments("checkers");
+//        assertEquals(2, comments.size());
+//
+//        assertEquals("checkers", comments.get(0).getGame());
+//        assertEquals("player1", comments.get(0).getPlayer());
+//        assertEquals("Nice game!", comments.get(0).getComment());
+//        assertEquals(date, comments.get(0).getCommentedOn());
+//
+//        assertEquals("checkers", comments.get(1).getGame());
+//        assertEquals("player2", comments.get(1).getPlayer());
+//        assertEquals("Awesome!", comments.get(1).getComment());
+//        assertEquals(date, comments.get(1).getCommentedOn());
+//
+//        commentService.reset();
+//    }
+//
+//    @Test
+//    public void resetComments() {
+//        commentService.reset();
+//
+//        Date date = new Date();
+//        commentService.addComment(new Comment("checkers", "player1", "Nice game!", date));
+//
+//        List<Comment> comments = commentService.getComments("checkers");
+//        assertEquals(1, comments.size());
+//
+//        commentService.reset();
+//
+//        comments = commentService.getComments("checkers");
+//        assertEquals(0, comments.size());
+//
+//        commentService.reset();
+//    }
+//}
