454380b82121445abcdf4d2d04cdb00e64205a65
Danylo Senchyshyn <danilsencisin@gmail.com>
2025-03-31 16:41:56 +0200

    Re-added only src and pom.xml

--- src/main/java/sk/tuke/gamestudio/game/checkers/core/GameState.java
+++
@@ -1,5 +0,0 @@
-package sk.tuke.gamestudio.game.checkers.core;
-
-public enum GameState {
-    PLAYING, WHITE_WON, BLACK_WON, DRAW;
-}
--- src/main/java/sk/tuke/gamestudio/game/checkers/core/Man.java
+++
@@ -1,7 +0,0 @@
-package sk.tuke.gamestudio.game.checkers.core;
-
-public class Man extends Tile {
-    public Man(TileState tileState) {
-        super(tileState);
-    }
-}
